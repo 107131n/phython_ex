6cd12c10add6a46399197865401f883e55f90c6e
107131n <ddolgi1206@nate.com>
2023-02-06 17:34:50 +0900

    카드

---
+++ 06_bookcard.py
@@ -0,0 +1,74 @@
+# 도서관리 프로그램
+# 데이터 구조는 리스트를 이용
+# 일련 번호, 책 이름, 출판사, ...
+# 1. 저장 2. 수정 3. 삭제 4. 리스트 5. 검색 6. 종료(Q)
+
+card =[]
+
+while True:
+    menu = input('''
+---------------------------------------------------
+1. 저장 2. 수정 3. 삭제 4. 리스트 5. 검색 6. 종료(Q)
+---------------------------------------------------
+>>> ''')
+    if menu == '1':
+        while True:
+            name = input('책 이름을 입력하세요. >>> ')
+            check = 0
+            for item in card:
+                if item[0] == name:
+                    check = 1
+            if check == 0:
+                break
+            print('중복인 이름이 있습니다.')
+
+        number = input('일련 번호를 입력하세요. >>> ')
+        publish = input('출판사를 입력하세요. >>> ')
+        card.append([name, number, publish])
+        print(card)
+        
+    elif menu == '2':
+        name = input('수정할 책 이름 >>> ')
+        idx = -1
+        for i, item in enumerate(card):
+            if item[0] == name:
+                idx = i
+                update = int(input('수정할 정보(1.name, 2. number, 3.publish)>>>'))
+                card[idx][update] = input('수정 내용 >>> ')
+                break
+                print(card[idx])
+            if idx == 1:
+                print('등록되지 않은 데이터입니다.')
+    elif menu == '3':
+        name = input('삭제할 이름 입력 >>> ')
+        delok = 0
+        for i, item in enumerate(card):
+            if item[0] == name:
+                print('삭제!')
+                del card[i]
+                delok = 1
+                break
+            if delok == 0:
+                print('등록되지 않은 데이터입니다.')
+    elif menu == '4':
+        for item in card:
+            print(f'''
+-----------------------------------------
+책    이름 : {item[0]}
+일련  번호 : {item[1]}
+출  판  사 : {item[2]}''')
+    elif menu == '5':
+        name = input('검색할 이름 입력 >>> ')
+        idx = -1
+        for i, item in enumerate(card):
+            if item[0] == name:
+            idx = i
+            print(item)
+            break
+        if idx == -1:
+            print('등록되지 않은 데이터입니다.')
+    elif menu in ('6', 'q', 'Q'):
+        print('프로그램 종료')
+        break
+    else:
+        print('메뉴를 잘못 선택하였습니다.')
