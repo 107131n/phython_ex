8e02fdb889ce11874ae6ad92ec13c9635fe1e7f9
107131n <ddolgi1206@nate.com>
2023-02-16 16:20:01 +0900

    custmor basic

--- 04_고객관리프로그램/customer_basic.py
+++ 04_고객관리프로그램/customer_basic.py
@@ -1,8 +1,11 @@
 #콘솔형 고객 정보 관리 시스템 구축
 
 import re
-custlist=[]
-page=-1
+custlist= [{'name': 'hong1', 'gender' : 'M', 'email': 'hong1@gmail.com', 'birthyear' : '2002'},
+          {'name': 'hong1', 'gender' : 'M', 'email': 'hong2@gmail.com', 'birthyear' : '2002'},
+          {'name': 'hong1', 'gender' : 'M', 'email': 'hong3@gmail.com', 'birthyear' : '2002'},
+          {'name': 'hong1', 'gender' : 'M', 'email': 'hong4@gmail.com', 'birthyear' : '2002'}]
+page=3
 
 while True:
     choice=input('''
@@ -59,35 +62,69 @@ while True:
 
 
     elif choice=="C":
-        print("현재 고객 정보 조회")
+        if page >= 0:
+            print(f'현재 페이지는 {page+1}페이지 입니다.')
+            print(custlist[page])
+        else:
+            print('입력된 정보가 없습니다.')
+
 
     elif choice == 'P':
-        print("이전 고객 정보 조회")
+        if page <= 0:
+            print('첫번째 페이지입니다.')
+            print(page)
+        else:
+            page -= 1
+            print(f'현재 페이지는 {page+1}페이지입니다.')
+            print(custlist[page])
+
 
     elif choice == 'N':
-        print("다음 고객 정보 조회")
+        if page >= len(custlist)-1:
+            print('마지막 페이지입니다.')
+            print(page)
+        else:
+            page += 1
+            print(f'현재 페이지는 {page+1}페이지입니다.')
+            print(custlist[page])
 
 
     elif choice=='D':
-        print("고객 정보 삭제")
-        key = email
-        if email == custlist(email):
-            del(email)
-            print("고객 정보 삭제")
+        email = input('삭제할 고객의 이메일을 입력하세요. >>> ').strip()
+        delok = 0
+        for idx, i in enumerate(custlist):
+          if i['email'] == email:
+            data = custlist.pop(idx) #del도 가능, pop안에 인덱스 넣는 것도 가능
+            print('{}님의 정보가 삭제되었습니다.'.format(data['name']))
             delok = 1
-        else:
+            break
+        if delok == 0:
             print('등록되지 않은 이메일입니다.')
-            delok = 0
-        pass
+        print(custlist)        
+
 
     elif choice=="U": 
-        # print("고객 정보 수정")
-        # key = email
-        # if email == a:
-        pass
+        email = input('수정할 고객의 이메일을 입력하세요. >>> ').strip()
+        idx = -1
+        for id, i in enumerate(custlist):
+            print(id,i)
+            if i['email'] == email:
+                print(i['email'])
+                idx = id
+                break
+        if idx == -1:
+            print('등록되지 않은 이메일입니다.')
+        else:
+            key = input('''
+다음 중 수정할 항목은(name, gender, birthyear)
+수정할 정보가 없으면 enter >>> ''')
+            if key in ('name','gender','birthyear'):
+                custlist[idx][key] = input('수정할 {}를 입력하세요.'.format(key))
+
     elif choice=="Q":
         print("프로그램 종료")
         break
+
     else:
         print('메뉴를 잘못 선택했습니다.')
 
